1beb9d655f52226dfe218c6320a625580b7bf984
alptangal <alptangal@gmail.com>
2024-10-04 00:00:21 +0700

    .

--- main.py
+++ main.py
@@ -90,88 +90,89 @@ async def getToken():
 @tasks.loop(seconds=.1)
 async def fetchData():
     global STEP,TOKEN,RESULT
-    try:
-        if int(str(round(datetime.now().timestamp())).split(',')[0])%10==0:
-            await RESULT['logsCh'].send(STEP)
-        done=False
-        print('fetchData is running')
-        print(STEP)
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.9',
-            }
-        url=f'https://main.v2.beatstars.com/musician?id={STEP}&fields=profile,user_contents,stats,bulk_deals,social_networks,email'
+    if STEP:
         try:
-            req=requests.get(url,headers=headers)
+            if int(str(round(datetime.now().timestamp())).split(',')[0])%10==0:
+                await RESULT['logsCh'].send(STEP)
+            done=False
+            print('fetchData is running')
+            print(STEP)
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.9',
+                }
+            url=f'https://main.v2.beatstars.com/musician?id={STEP}&fields=profile,user_contents,stats,bulk_deals,social_networks,email'
+            try:
+                req=requests.get(url,headers=headers)
+            except Exception as err:
+                await RESULT['logsCh'].send(err)
+                pass
+            data=req.json()
+            print(data)
+            if data['response']['type']=='success':
+                data=data['response']['data']
+                headers={
+                    'authorization':'Bearer '+TOKEN['token']
+                }
+                url='https://open.larksuite.com/open-apis/sheets/v3/spreadsheets/ObB8syGTHhzLpjtkIuvlL893gcf/sheets/query'
+                sheets=[]
+                req=requests.get(url,headers=headers)
+                if req.status_code<400:
+                    js=req.json()
+                    if js['code']==0:
+                        for item in js['data']['sheets']:
+                            sheets.append(item)
+                for sheet in sheets:
+                    if sheet['grid_properties']['row_count']<71428:
+                        url='https://open.larksuite.com/open-apis/sheets/v2/spreadsheets/ObB8syGTHhzLpjtkIuvlL893gcf/values_append'
+
+                        raw=[
+                            data['profile']['user_id'],
+                            data['email'],
+                            data['profile']['first_name'],
+                            data['profile']['last_name'],
+                            data['profile']['display_name'],
+                            data['profile']['avatar']['original'] if data['profile']['avatar'] else None,
+                            data['profile']['location'],
+                            data['profile']['permalink'],
+                            data['profile']['relative_uri'],
+                            data['profile']['beatstars_uri'],
+                            data['profile']['propage_uri'],
+                            data['profile']['user_type'],
+                            data['profile']['subscription_id'],
+                            data['profile']['subscription_type'],
+                            str(data['profile']['verified']),
+                            data['profile']['biography'],
+                            data['profile']['biography_summary'],
+                            data['stats']['followers'],
+                            data['stats']['plays'],
+                            data['stats']['tracks'],
+                            data['stats']['following']
+                        ]
+                        if data['social_networks']:
+                            for item in data['social_networks']:
+                                raw.append(item['uri'])
+                        data={
+                            "valueRange": {
+                                "range": sheet['sheet_id'],
+                                "values": [
+                                    raw
+                                ]
+                            }
+                        }
+                        try:
+                            req=requests.post(url,headers=headers,json=data)
+                        except Exception as err:
+                            await RESULT['logsCh'].send(err)
+                            pass
+                        if(req.status_code<400):
+                            print('Created new record success')
+                            STEP+=1
+                            done=True
+                            break
+            if done==False and 'musician not found' in data['response']['data']['message'].lower():
+                STEP+=1
         except Exception as err:
             await RESULT['logsCh'].send(err)
             pass
-        data=req.json()
-        print(data)
-        if data['response']['type']=='success':
-            data=data['response']['data']
-            headers={
-                'authorization':'Bearer '+TOKEN['token']
-            }
-            url='https://open.larksuite.com/open-apis/sheets/v3/spreadsheets/ObB8syGTHhzLpjtkIuvlL893gcf/sheets/query'
-            sheets=[]
-            req=requests.get(url,headers=headers)
-            if req.status_code<400:
-                js=req.json()
-                if js['code']==0:
-                    for item in js['data']['sheets']:
-                        sheets.append(item)
-            for sheet in sheets:
-                if sheet['grid_properties']['row_count']<71428:
-                    url='https://open.larksuite.com/open-apis/sheets/v2/spreadsheets/ObB8syGTHhzLpjtkIuvlL893gcf/values_append'
-
-                    raw=[
-                        data['profile']['user_id'],
-                        data['email'],
-                        data['profile']['first_name'],
-                        data['profile']['last_name'],
-                        data['profile']['display_name'],
-                        data['profile']['avatar']['original'] if data['profile']['avatar'] else None,
-                        data['profile']['location'],
-                        data['profile']['permalink'],
-                        data['profile']['relative_uri'],
-                        data['profile']['beatstars_uri'],
-                        data['profile']['propage_uri'],
-                        data['profile']['user_type'],
-                        data['profile']['subscription_id'],
-                        data['profile']['subscription_type'],
-                        str(data['profile']['verified']),
-                        data['profile']['biography'],
-                        data['profile']['biography_summary'],
-                        data['stats']['followers'],
-                        data['stats']['plays'],
-                        data['stats']['tracks'],
-                        data['stats']['following']
-                    ]
-                    if data['social_networks']:
-                        for item in data['social_networks']:
-                            raw.append(item['uri'])
-                    data={
-                        "valueRange": {
-                            "range": sheet['sheet_id'],
-                            "values": [
-                                raw
-                            ]
-                        }
-                    }
-                    try:
-                        req=requests.post(url,headers=headers,json=data)
-                    except Exception as err:
-                        await RESULT['logsCh'].send(err)
-                        pass
-                    if(req.status_code<400):
-                        print('Created new record success')
-                        STEP+=1
-                        done=True
-                        break
-        if done==False and 'musician not found' in data['response']['data']['message'].lower():
-            STEP+=1
-    except Exception as err:
-        await RESULT['logsCh'].send(err)
-        pass
 client.run(os.environ.get('t'))
 
